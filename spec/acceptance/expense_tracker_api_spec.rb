9c1325739533dc4cfbbc349553746643a591c231
Ryan Brown <ryan.brown@barre3.com>
2019-05-22 17:58:04 -0700

    add Ledger class, api_spec.rb

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,18 +10,19 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
+    let(:parsed) { JSON.parse(last_response.body) }
+
     def post_expense(expense)
       post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
-      parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
       expense.merge('id' => parsed['expense_id'])
     end
 
     it "records submitted expenses" do
       pending "Need to persist expenses"
-        
+
       coffee = post_expense(
         'payee' => 'Starbucks',
         'amount' => 5.75,
@@ -43,8 +44,7 @@ module ExpenseTracker
       get 'expenses/2017-06-10'
       expect(last_response.status).to eq(200)
 
-      expenses = JSON.parse(last_response.body)
-      expect(expenses).to contain_exactly(coffee, 200)
+      expect(parsed).to contain_exactly(coffee, 200)
     end
   end
 end
